afec965bc86ac6d6ad585cb0fb31137b8d098b23
Nataly Nazario Quina <nataly.quina@gmail.com>
2020-02-10 22:01:10 -0300

    Atualizaçao table

---
+++ database/migrations/2020_01_31_155007_create_contatos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contatos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('cliente', 150);
+            $table->string('empresa', 150)->nullable();
+            $table->string('email', 150)->unique();
+            $table->string('telefone', 150);
+            $table->string('vendedor', 150);
+            $table->date('validade', 150)->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contatos');
+    }
+}
